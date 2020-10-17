e8ae93cd51cddac5c056157f851e1ab0a7aad08c
NAThompson <nathompson7@protonmail.com>
2020-10-17 13:52:29 -0400

    Add parallel benchmarks.

--- benchmarks/bench.cpp
+++ benchmarks/bench.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <random>
+#include <algorithm>
+#include <execution>
 #include <benchmark/benchmark.h>
 
 template<typename Real>
@@ -29,6 +31,7 @@ void DotProduct(benchmark::State& state)
     {
         benchmark::DoNotOptimize(dot_product(a.data(), b.data(), a.size()));
     }
+    state.SetBytesProcessed(state.range(0)*sizeof(Real));
     state.counters["n"] = state.range(0);
     state.SetComplexityN(state.range(0));
 }
@@ -36,5 +39,54 @@ void DotProduct(benchmark::State& state)
 BENCHMARK_TEMPLATE(DotProduct, float)->RangeMultiplier(2)->Range(1<<3, 1<<18)->Complexity(benchmark::oN)->Unit(benchmark::kMicrosecond);
 BENCHMARK_TEMPLATE(DotProduct, double)->DenseRange(1024, 1024*1024, 1024)->Complexity(benchmark::oN)->Unit(benchmark::kMicrosecond);
 BENCHMARK_TEMPLATE(DotProduct, long double)->RangeMultiplier(2)->Range(1<<3, 1<<18)->Complexity(benchmark::oN)->Unit(benchmark::kMicrosecond);
+
+template<class Real>
+void Sort(benchmark::State& state)
+{
+    std::vector<Real> a(state.range(0));
+    std::random_device rd;
+    std::uniform_real_distribution<Real> unif(-1,1);
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        a[i] = unif(rd);
+    }
+
+    for (auto _ : state)
+    {
+        std::sort(a.begin(), a.end());
+        benchmark::DoNotOptimize(a[0]);
+        state.PauseTiming();
+        std::shuffle(a.begin(), a.end(), rd);
+        state.ResumeTiming();
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK_TEMPLATE(Sort, double)->RangeMultiplier(2)->Range(1<<7, 1<<19)->Complexity()->Unit(benchmark::kMicrosecond)->UseRealTime();
+
+template<class Real>
+void SortParallel(benchmark::State& state)
+{
+    std::vector<Real> a(state.range(0));
+    std::random_device rd;
+    std::uniform_real_distribution<Real> unif(-1,1);
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        a[i] = unif(rd);
+    }
+
+    for (auto _ : state)
+    {
+        std::sort(std::execution::par_unseq, a.begin(), a.end());
+        benchmark::DoNotOptimize(a[0]);
+        state.PauseTiming();
+        std::shuffle(a.begin(), a.end(), rd);
+        state.ResumeTiming();
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK_TEMPLATE(SortParallel, double)->RangeMultiplier(2)->Range(1<<7, 1<<19)->Complexity()->Unit(benchmark::kMicrosecond)->UseRealTime();
+
 
 BENCHMARK_MAIN();

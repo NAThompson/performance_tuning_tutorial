698e31a53d8bb92559ddbf197cd63c4dbc36611e
Nick Thompson <nathompson7@protonmail.com>
2021-01-07 15:40:25 -0500

    We won't have time for parallel google/benchmark.

--- benchmarks/bench.cpp
+++ benchmarks/bench.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <random>
 #include <algorithm>
-#include <execution>
 #include <chrono>
 #include <benchmark/benchmark.h>
 
@@ -41,58 +40,6 @@ void DotProduct(benchmark::State& state)
 BENCHMARK_TEMPLATE(DotProduct, float)->RangeMultiplier(2)->Range(1<<3, 1<<22)->Complexity(benchmark::oN)->Unit(benchmark::kMicrosecond);
 BENCHMARK_TEMPLATE(DotProduct, double)->RangeMultiplier(2)->Range(1<<6, 1<<22)->Complexity(benchmark::oN)->Unit(benchmark::kMicrosecond);
 BENCHMARK_TEMPLATE(DotProduct, long double)->RangeMultiplier(2)->Range(1<<3, 1<<18)->Complexity()->Unit(benchmark::kMicrosecond);
-
-template<class Real>
-void Sort(benchmark::State& state)
-{
-    std::vector<Real> a(state.range(0));
-    std::random_device rd;
-    std::uniform_real_distribution<Real> unif(-1,1);
-    for (size_t i = 0; i < a.size(); ++i)
-    {
-        a[i] = unif(rd);
-    }
-
-    for (auto _ : state)
-    {
-        auto start = std::chrono::high_resolution_clock::now();
-        std::sort(a.begin(), a.end());
-        auto end = std::chrono::high_resolution_clock::now();
-        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-        state.SetIterationTime(elapsed_seconds.count());
-        benchmark::DoNotOptimize(a[0]);
-        std::shuffle(a.begin(), a.end(), rd);
-    }
-    state.SetComplexityN(state.range(0));
-}
-
-BENCHMARK_TEMPLATE(Sort, double)->RangeMultiplier(2)->Range(1<<5, 1<<16)->Complexity()->Unit(benchmark::kMicrosecond)->UseManualTime();
-
-template<class Real>
-void SortParallel(benchmark::State& state)
-{
-    std::vector<Real> a(state.range(0));
-    std::random_device rd;
-    std::uniform_real_distribution<Real> unif(-1,1);
-    for (size_t i = 0; i < a.size(); ++i)
-    {
-        a[i] = unif(rd);
-    }
-
-    for (auto _ : state)
-    {
-        auto start = std::chrono::high_resolution_clock::now();
-        std::sort(std::execution::par_unseq, a.begin(), a.end());
-        auto end = std::chrono::high_resolution_clock::now();
-        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-        state.SetIterationTime(elapsed_seconds.count());
-        benchmark::DoNotOptimize(a[0]);
-        std::shuffle(a.begin(), a.end(), rd);
-    }
-    state.SetComplexityN(state.range(0));
-}
-
-BENCHMARK_TEMPLATE(SortParallel, double)->RangeMultiplier(2)->Range(1<<5, 1<<16)->Complexity()->Unit(benchmark::kMicrosecond)->UseManualTime();
 
 
 template <typename T>
